f8be8db498f14a31956244a85c85604565af0002
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-10 19:34:26 -0800

    added components, pages, and installed socket.io--client

--- simple-node-chat-server/src/App.js
+++ simple-node-chat-server/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+import ChatRoom from './pages/ChatRoom'
+import ClearChat from './pages/ClearChat'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App" style={{justifyContent:'center', display: 'flex'}}>
+
+        <Switch>
+
+          <Route exact path="/">
+            <ChatRoom/>
+          </Route>
+
+          <Route exact path="/clearChat">
+            <ClearChat/>
+          </Route>
+
+
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
