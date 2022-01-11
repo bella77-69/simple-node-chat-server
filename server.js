668f00ba954fbb04d8695a64a56e07f4c051e705
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-10 19:28:45 -0800

    added client side

--- server.js
+++ server.js
@@ -30,5 +30,53 @@ var chatRoomData = []
 var connectedClients = {}
 
 io.on('connection', (client) => {
+    console.log('New Client connected');
+
+    //Client Sent a message
+    client.on('SendMessage', (messageData) => {
+        chatRoomData.push(messageData)
+        sendUpdatedChatRoomData(client)
+    })
+
+    //Client entered the Chat Room
+    client.on('UserEnteredRoom', (userData) => {
+        var enteredRoomMessage = {message: `${userData.username} has entered the chat`, username: '', userID: 0, timeStamp: null}
+        chatRoomData.push(enteredRoomMessage)
+        sendUpdatedChatRoomData(client)
+        connectedClients[client.id] = userData
+    })
+
+    //Creating identity for new connected user
+    client.on('CreateUserData', () => {
+        let userID = uuid();
+        let username = uniqueNamesGenerator({dictionaries: [adjectives, names] });
+        var userData = {userID: userID, username: username}
+        client.emit('SetUserData', userData)
+    })
+
+    //Player Disconnecting from chat room
+    client.on('disconnecting', (data) => {
+        console.log("Client disconnecting...");
     
+        if(connectedClients[client.id]){
+          var leftRoomMessage = {message: `${connectedClients[client.id].username} has left the chat`, username: "", userID: 0, timeStamp: null}
+          chatRoomData.push(leftRoomMessage)
+          sendUpdatedChatRoomData(client)
+          delete connectedClients[client.id]
+        }
+    
+      });
+
+      //Clearing Chat Room Data from server
+      client.on('ClearChat', () => {
+          chatRoomData=[]
+          console.log(chatRoomData)
+          sendUpdatedChatRoomData(client)
+      })
 })
+
+    //Sending Update Chat Room Data to all Connected Clients
+    function sendUpdatedChatRoomData(client) {
+        client.emit("RetrieveChatRoomData", chatRoomData)
+        client.broadcast.emit("RetrieveChatRoomData", chatRoomData)
+    }
