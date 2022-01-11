20c064b083edeaf110b4d76af796bff3cb06387f
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-10 18:45:15 -0800

    server.js code

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var uuid = require('uuid-random');
+
+const { uniqueNamesGenerator, adjectives, colors, animals, names } = require('unique-names-generator');
+
+var PORT = process.env.PORT || 8080;
+
+var server = app.listen(PORT, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Listening at http://%s:%s', 'localhost:', port);
+});
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Orgin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+var io = require('socket.io')(server)
